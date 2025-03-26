45e557a6da26b29d162343600ba5d49b54091f62
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2025-03-26 19:41:40 +0900

    refact/ refact Bag icon

--- src/app/components/Checkout/ClosedCheckout/index.tsx
+++ src/app/components/Checkout/ClosedCheckout/index.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
-import Bag from "/public/bag.svg"
 import { BagWrapped } from "./styles"
 import { useDispatch } from 'react-redux';
 import { openCheckout } from "@/lib/features/checkout/checkoutSlice";
 import { AppDispatch } from '@/lib/store';
+import { Bag } from "./Bag";
+
 
 
 export default function ClosedCheckout() {
@@ -12,7 +12,7 @@ export default function ClosedCheckout() {
 
     return (
         <BagWrapped onClick={()=>dispatch(openCheckout())} >
-            <Image src={Bag} width={33} height={33} alt={"bag"} />
+            <Bag />
             <p>0</p>
         </BagWrapped>
     )
