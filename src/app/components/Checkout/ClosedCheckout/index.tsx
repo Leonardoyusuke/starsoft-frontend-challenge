81df14b110dc575acb5a488c3a68a779e9e73d4f
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2025-03-26 19:30:01 +0900

    refact/ Import Image

--- src/app/components/Checkout/ClosedCheckout/index.tsx
+++ src/app/components/Checkout/ClosedCheckout/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Bag from '/public/bag.svg'
 import { BagWrapped } from "./styles"
 import { useDispatch } from 'react-redux';
 import { openCheckout } from "@/lib/features/checkout/checkoutSlice";
@@ -11,7 +12,7 @@ export default function ClosedCheckout() {
 
     return (
         <BagWrapped onClick={()=>dispatch(openCheckout())} >
-            <Image src="bag.svg" width={33} height={33} alt={"bag"} />
+            <Image src={Bag} width={33} height={33} alt={"bag"} />
             <p>0</p>
         </BagWrapped>
     )
