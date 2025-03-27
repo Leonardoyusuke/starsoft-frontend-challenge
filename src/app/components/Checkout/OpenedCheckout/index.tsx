4975a654c651ddde904572d42183f1b998812544
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2025-03-27 16:48:22 +0900

    refact/ shop-chat motion animation with framer-motion

--- src/app/components/Checkout/OpenedCheckout/index.tsx
+++ src/app/components/Checkout/OpenedCheckout/index.tsx
@@ -1,20 +1,36 @@
-import { Container, Top } from "./styles";
+'use client'
+
+import { Top, MotionContainer } from "./styles";
 import Image from "next/image";
 import backArrow from "/public/BackArrow.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { closeCheckout } from "@/lib/features/checkout/checkoutSlice";
 import { AppDispatch, RootState } from '@/lib/store';
+import { AnimatePresence } from "framer-motion";
 
 export default function OpenedCheckout() {
   const dispatch = useDispatch<AppDispatch>();
   const isOpen = useSelector((state: RootState) => state.checkout.isOpen);
 
   return (
-    <Container $isOpen={isOpen}>
-      <Top>
-        <Image src={backArrow} alt="BackArrow" onClick={() => dispatch(closeCheckout())} />
-        <p>Mochila de Compras</p>
-      </Top>
-    </Container>
+    <AnimatePresence>
+      {isOpen && (
+        <MotionContainer
+          initial={{ x: '100%', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: '100%', opacity: 0 }}
+          transition={{ duration: 0.3, ease: "easeOut" }}
+        >
+          <Top>
+            <Image
+              src={backArrow}
+              alt="BackArrow"
+              onClick={() => dispatch(closeCheckout())}
+            />
+            <p>Mochila de Compras</p>
+          </Top>
+        </MotionContainer>
+      )}
+    </AnimatePresence>
   );
 }
--- src/app/components/Checkout/OpenedCheckout/styles.ts
+++ src/app/components/Checkout/OpenedCheckout/styles.ts
@@ -1,37 +1,30 @@
-'use client'
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-import styled from "styled-components"
-
-interface ContainerProps {
-    $isOpen: boolean;
-  }
-  
-  export const Container = styled.div<ContainerProps>`
-    position: absolute;
-    right: 0px;
-    top: 0px;
-    width: 679px;
-    height: 1117px;
-    border-radius: 5px;
-    background-color: #232323;
-    z-index: 1;
-  
-    transition: transform 300ms ease-out, opacity 300ms ease-out;
-    transform: ${({ $isOpen }) => ($isOpen ? 'translateX(0)' : 'translateX(100%)')};
-    opacity: ${({ $isOpen }) => ($isOpen ? 1 : 0)};
-    pointer-events: ${({ $isOpen }) => ($isOpen ? 'auto' : 'none')};
-  `;
+export const MotionContainer = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 679px;
+  height: 1117px;
+  border-radius: 5px;
+  background-color: gray;
+  z-index: 1;
+`;
 
 export const Top = styled.div`
-position: relative;
-img{
+  position: relative;
+
+  img {
     position: absolute;
     width: 60px;
     height: 60px;
     top: 60px;
     left: 40px;
-}
-p{
+    cursor: pointer;
+  }
+
+  p {
     position: absolute;
     width: 253px;
     height: 26px;
@@ -41,7 +34,6 @@ p{
     font-weight: 500;
     font-size: 24px;
     line-height: 110%;
-    letter-spacing: 0;
     color: #FFFFFF;
-}
-`
+  }
+`;
