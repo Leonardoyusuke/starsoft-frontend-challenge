c8a962a9d20e15535071baf34c8b149bde9227a6
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2025-03-27 16:39:10 +0900

    /Footer done

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Footer() {
+    return (
+        <Container>
+            <p>STARSOFT © TODOS OS DIREITOS RESERVADOS</p>
+        </Container>
+    )
+}
---
+++ src/app/components/Footer/styles.ts
@@ -0,0 +1,24 @@
+'use client'
+import styled from "styled-components";
+
+export const Container = styled.div`
+margin-top: 100px;
+align-items: center;
+justify-content: center;
+display: flex;
+width: 100%;
+height: 76px;
+p{
+    width: 386;
+    height: 26;
+    top: 25px;
+    left: 671px;
+    font-family: IBM Plex Sans;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 26px;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    color: #84848484;
+}
+`
