311042b62e0398c8f77e4559691c40d6d16871d4
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2025-03-31 12:07:56 +0900

    refact/ product page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -4,14 +4,15 @@ import ProductPage from './product';
 type PageProps = {
   params: Promise<{ slug: string }>;
 };
-export default async function Page(props: Promise<PageProps>) {
-  const { params } = await props;
 
+export default async function Page(props: PageProps) {
+  const { params } = await props;
   return <ProductPage slug={(await params).slug} />;
 }
+
 export async function generateMetadata(props: PageProps): Promise<Metadata> {
-  const params = await props.params;
-  const slug = params.slug.replace(/-/g, ' ');
+  const { params } = await props;
+  const slug = (await params).slug.replace(/-/g, ' ');
 
   return {
     title: `${slug.charAt(0).toUpperCase() + slug.slice(1)} | Starsoft`,
